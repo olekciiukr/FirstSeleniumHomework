c4b18d8c9d62f5e5146244b628138ce3e66fe7ec
Aleksey <olekcii,ukr@gmail.com>
2026-04-20 23:42:24 +0200

    refactoring helpers

--- src/test/java/homework/AddItemToCartTest.java
+++ src/test/java/homework/AddItemToCartTest.java
@@ -7,9 +7,10 @@ import org.testng.annotations.Test;
 public class AddItemToCartTest extends TestBase {
 
     @Test
-    public void addItemToCartTest() {
+    public void addItemToCartTest() throws InterruptedException {
         app.getUser().login();
         app.getBase().click(By.xpath("//a[text()='14.1-inch Laptop']/../../..//input[@value='Add to cart']"));
+        driver.get("https://demowebshop.tricentis.com/cart");
         app.getBase().click(By.cssSelector(".cart-label"));
         Assert.assertTrue(app.getBase().isPresent(By.xpath("//a[text()='14.1-inch Laptop']")));
 
